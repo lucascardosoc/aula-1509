c6d8c612ab7b2488378a65d55fac72b1fb972bcc
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-09-19 22:11:50 -0300

    Comuns - JS

--- js/comuns.js
+++ js/comuns.js
@@ -46,3 +46,7 @@ console.log(`${n1} + ${n2} = ${n1 + n2}`)
 console.log(`${n1} - ${n2} = ${n1 - n2}`)
 console.log(`${n1} * ${n2} = ${n1 * n2}`)
 console.log(`${n1} / ${n2} = ${n1 / n2}`)
+
+{
+  ()
+}
